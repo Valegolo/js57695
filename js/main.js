0dc3a2856537d65d8a2173cf209bd546e8bfd7e9
valegolo <valentinagolo98@hotmail.com>
2024-05-13 11:58:34 -0500

    correcciones tutor

--- js/main.js
+++ js/main.js
@@ -1,21 +1,29 @@
-const superheroes = ['Superman', 'Spiderman', 'Batman', 'Mujer Maravilla', 'Iron Man', 'Capitan America', 'Hulk', 'Thor', 'Black Widow', 'Flash'];
-    const secretHero = superheroes[Math.floor(Math.random() * superheroes.length)];
-    let attempts = 0;
+setTimeout(function() {
+    const superheroes = ['Superman', 'Spiderman', 'Batman', 'Mujer Maravilla', 'Iron Man', 'Capitan America', 'Hulk', 'Thor', 'Black Widow', 'Flash'];
 
-    function checkGuess() {
-      let guess = document.getElementById('guessInput').value.trim().toLowerCase();
+    function playGame() {
+        const secretHero = superheroes[Math.floor(Math.random() * superheroes.length)]; 
+        let attempts = 0; 
+        function checkGuess() {
+            let guess = prompt('Introduce un nombre de superhéroe:');
+            while (!guess || !superheroes.includes(guess.trim())) {
+                if (!guess) {
+                    alert("Escribe el nombre de un superhéroe.");
+                } else {
+                    alert("El término introducido no es un superhéroe válido :( Intentalo de nuevo.");
+                }
+                guess = prompt('Introduce un nombre de superhéroe:');
+            }
+            attempts++; 
+            if (guess.toLowerCase() !== secretHero.toLowerCase()) {
+                checkGuess(); 
+            }
+        }
 
-      while (!guess) {
-        guess = prompt('Por favor, introduce un nombre de superhéroe:');
-      }
+        checkGuess();
 
-      attempts++;
-
-      if (guess === secretHero.toLowerCase()) {
-        alert(`¡Felicidades! Adivinaste al superhéroe secreto ${secretHero} en ${attempts} intentos.`);
-      } else {
-        alert('Ese no es el superhéroe secreto. ¡Inténtalo de nuevo!');
-      }
+        alert("¡Felicidades! Adivinaste el superhéroe en " + attempts + " intentos.");
     }
-    
-   
+
+    playGame();
+}, 1000);
