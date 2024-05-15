7f4e0ec2a05efd9ce220580c7648a10d010d54b8
valegolo <valentinagolo98@hotmail.com>
2024-05-15 18:50:17 -0500

    arreglos

--- js/main.js
+++ js/main.js
@@ -2,21 +2,29 @@ setTimeout(function() {
     const superheroes = ['Superman', 'Spiderman', 'Batman', 'Mujer Maravilla', 'Iron Man', 'Capitan America', 'Hulk', 'Thor', 'Black Widow', 'Flash'];
 
     function playGame() {
-        const secretHero = superheroes[Math.floor(Math.random() * superheroes.length)]; 
-        let attempts = 0; 
+        const secretHero = superheroes[Math.floor(Math.random() * superheroes.length)];
+        let attempts = 0;
+        
         function checkGuess() {
             let guess = prompt('Introduce un nombre de superhéroe:');
-            while (!guess || !superheroes.includes(guess.trim())) {
-                if (!guess) {
-                    alert("Escribe el nombre de un superhéroe.");
-                } else {
-                    alert("El término introducido no es un superhéroe válido :( Intentalo de nuevo.");
-                }
-                guess = prompt('Introduce un nombre de superhéroe:');
+            if (!guess) {
+                alert("Por favor, escribe el nombre de un superhéroe.");
+                checkGuess();
+                return;
             }
-            attempts++; 
-            if (guess.toLowerCase() !== secretHero.toLowerCase()) {
-                checkGuess(); 
+            guess = guess.trim();
+            const guessLowerCase = guess.toLowerCase();
+            if (!superheroes.map(hero => hero.toLowerCase()).includes(guessLowerCase)) {
+                alert("Por favor, escribe un superhéroe válido.");
+                checkGuess();
+                return;
+            }
+            attempts++;
+            if (guessLowerCase === secretHero.toLowerCase()) {
+                alert("¡Felicidades! ¡Has adivinado al superhéroe ganador!");
+            } else {
+                alert("Sigue intentando. No has adivinado al superhéroe ganador.");
+                checkGuess();
             }
         }
 
@@ -27,3 +35,4 @@ setTimeout(function() {
 
     playGame();
 }, 1000);
+
